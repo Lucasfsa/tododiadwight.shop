dfe8795af927057434ee6729eff382672e85bb16
Lucasfsa <lucas792azevedo@gmail.com>
2024-02-11 16:32:25 -0300

    config vercel analystics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
 import TopBar from './components/TopBar.vue'
-import Footer from './components/Footer.vue'
-import RightBar from './components/RightBar.vue'
-import LeftBar from './components/LeftBar.vue'
+
 import Body from './components/Body.vue'
+import { inject } from '@vercel/analytics';
 
 </script>
 
@@ -19,12 +18,9 @@ import Body from './components/Body.vue'
        </div>
        
      </main>
-<!--  
-     <footer>
-         <Footer></Footer>
-     </footer> -->
- 
   </div>
+
+  <inject></inject>
  </template>
  
 
